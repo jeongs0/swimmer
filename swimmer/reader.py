2371d48cec6979454688dabd9198787668c212dc
Sukyoung Jeong <m@eil.me>
2012-04-15 13:15:48 +0900

    first commit

---
+++ swimmer/reader.py
@@ -0,0 +1,33 @@
+import os
+from codecs import open
+from markdown import Markdown
+
+class POST(object):
+	def __init__(self,title,content,link,pubdate):
+		self.title=title
+		self.content=content
+		self.link=link
+		self.pubdate=pubdate
+
+def getpost(postdir):
+    posts=[]
+    data=['title', 'link', 'pubdate']
+    empdata=['','','']
+    md=Markdown(extensions=['meta'])
+    for path,_,files in os.walk(postdir):
+        os.chdir(path)
+        for filename in files:
+            with open(filename, 'r', 'utf-8') as f:
+                content=md.convert(f.read())
+                print content
+                print type(md.Meta)
+                print type(data)
+                for i in xrange(2):
+                    if data[i] in md.Meta:
+                        empdata[i]=md.Meta[data[i]]
+                        else:
+                            empdata[i]=""
+                print empdata
+            post=POST(empdata[0], content, empdata[1], empdata[2])
+            posts.append(post)
+    return posts
